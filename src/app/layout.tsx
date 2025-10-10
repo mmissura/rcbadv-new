454f153d03f60cc75e054257692c8753c5a4a5ac
mmissura <mmissura@gmail.com>
2025-10-10 13:42:46 -0300

    feat: change color menu mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,31 +53,53 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`${be_vietnam.className} antialiased`}>
-        {/* Google Ads - Script */}
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-N2VPDDSK');
+      `}
+        </Script>
+
+        {/* Google tag (gtag.js) */}
         <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-G9EP990P94"
           strategy="afterInteractive"
         />
-        <Script id="google-tag-manager" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-            // Google Analytics 4
-            gtag('config', 'G-G9EP990P94');
+        // Google Analytics 4
+        gtag('config', 'G-G9EP990P94');
 
-            // Google Ads
-            gtag('config', 'AW-17439504834');            
+        // Google Ads
+        gtag('config', 'AW-17439504834');
 
-            // Google Tag Manager
+        // Google Tag Manager
             gtag('config', 'GTM-N2VPDDSK');
-            
-
-          `}
+      `}
         </Script>
+      </head>
+
+      <body>
+        <body className={`${be_vietnam.className} antialiased`}></body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N2VPDDSK"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
 
         <Navbar />
         {children}
